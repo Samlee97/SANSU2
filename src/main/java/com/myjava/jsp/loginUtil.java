9bdc18cbbe0431abc5e07798b85e25ec6e10deb6
Samlee97 <samleemohanty97@gmail.com>
2019-07-12 15:11:20 +0530

    frst commit

---
+++ src/main/java/com/myjava/jsp/loginUtil.java
@@ -0,0 +1,9 @@
+package com.myjava.jsp;
+
+public class loginUtil {
+	
+public static String makeItLower(String username)
+{
+	return username.toLowerCase();
+}
+}
---
+++ src/test/java/com/myjava/jsp/loginUtilTest.java
@@ -0,0 +1,24 @@
+package com.myjava.jsp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class loginUtilTest {
+
+	@Test
+	public void test() {
+		loginUtil login = new loginUtil();
+		String res= login.makeItLower("SAMLEE");
+		Assert.assertEquals("samlee", res);
+	}
+    
+	@Test
+	public void test123() {
+		loginUtil login = new loginUtil();
+		String res1=login.makeItLower("SAMLEE");
+		Assert.assertEquals("SUDIPA", res1);
+	}
+}
